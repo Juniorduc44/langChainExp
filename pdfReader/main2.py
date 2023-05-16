23a9beb64d8f478760e769bc1d14f81bff7111ac
Juniorduc44 <Juniorduc44@gmail.com>
2023-05-15 21:50:31 -0700

    updated

--- pdfReader/main2.py
+++ pdfReader/main2.py
@@ -3,10 +3,20 @@ from pypdf import PdfReader
 reader = PdfReader("file1.pdf")
 number_of_pages = len(reader.pages)
 number = number_of_pages - 1
-page = reader.pages[0-number_of_pages]
-text = page.extract_text()
+list= []
+count =0
+for i in range(10):
+    count+=1
+    page = reader.pages[count]
+    text = page.extract_text()
+    list.append(text)
+
 print(f"Number of pages: {number_of_pages}")
 print("")
 print("\n")
 print("The following are the content of the PDF: ")
-print(text)
+
+for i in list:
+    print(i)
+
+

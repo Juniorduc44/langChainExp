5fb8c93af1f011dd26710dddfb3afdbaf98d0c53
Juniorduc44 <Juniorduc44@gmail.com>
2023-05-15 20:59:46 -0700

    updated

--- main1.py
+++ main1.py
@@ -1,3 +1,26 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from dotenv import find_dotenv, load_dotenv
+from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+load_dotenv(find_dotenv())
+embeddings = OpenAIEmbeddings()
+
+
+loader = YoutubeLoader.from_youtube_url(input("Enter a youtube url: "))
+transcript = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+docs = text_splitter.split_documents(transcript)
+
+db = FAISS.from_documents(docs, embeddings)
+
+
+
+print("These are the documents:")
+print(docs)
+print("\n\n")
+print("This is the Database:\n")
+print(db)
+
